1ff84a88865f48771fa12cbed2ba141da19868a1
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-20 13:29:09 +0200

    01-first-flight.spec.js

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,26 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  });
+
+  it('should have a form', () => {
+    cy.get('form').should('exist');
+  });
+
+  it('should have a new item input', () => {
+    cy.get('[data-test="new-item-input"]').should('exist');
+  });
+
+  it('should have an add item button', () => {
+    cy.get('[data-test="add-item"]').should('be.disabled');
+  });
+
+  it('should add new item named "some item"', () => {
+    cy.get('[data-test="new-item-input"]').type('some item');
+    cy.get('[data-test="add-item"]').should('be.enabled');
+    cy.get('[data-test="add-item"]').click();
+    cy.get('[data-test="items-unpacked"]').contains('some item');
+  });
+});
