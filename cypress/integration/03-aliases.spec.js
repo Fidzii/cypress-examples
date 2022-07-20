b4f338f013de4d45f0f9665790ca474a6d735fa0
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-20 18:58:13 +0200

    03-aliases.spec.js

--- cypress/integration/03-aliases.spec.js
+++ cypress/integration/03-aliases.spec.js
@@ -3,5 +3,16 @@
 describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
+    cy.get('[data-test="filter-items"]').as('filterInput');
+    cy.get('[data-test="items"] li').as('items');
+  });
+
+  it('should contain only matching items', () => {
+    const phrase = 'Tooth';
+
+    cy.get('@filterInput').type(phrase);
+    cy.get('@items').each((item) => {
+      cy.wrap(item).should('contain.text', phrase).should('not.contain.text', 'Hoodie');
+    });
   });
 });
