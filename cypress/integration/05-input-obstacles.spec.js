32a3d2881b5d1877af1af953f77367e6bc35f743
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-21 15:49:15 +0200

    05-input-obstacles.spec.js

--- cypress/integration/05-input-obstacles.spec.js
+++ cypress/integration/05-input-obstacles.spec.js
@@ -13,38 +13,42 @@ describe('Input obstacles', () => {
   });
 
   it('should control a select input', () => {
-    cy.get('[data-test="select-input"]');
-    cy.get('[data-test="select-result"]');
+    cy.get('[data-test="select-input"]').select('Hulk');
+    cy.get('[data-test="select-result"]').contains('Hulk');
   });
 
   it('should find and control a checkbox input', () => {
-    cy.get('[data-test="checkbox-tomato"]');
     cy.get('[data-test="checkbox-result"]').contains('(None)');
-    cy.get('[data-test="checkbox-result"]');
+    cy.get('[data-test="checkbox-tomato"]').should('not.be.checked').check().should('be.checked');
+    cy.get('[data-test="checkbox-result"]').contains('Tomato');
   });
 
   it('should find and control a radio input', () => {
-    cy.get('[data-test="radio-ringo"]');
-    cy.get('[data-test="radio-result"]');
+    cy.get('[data-test="radio-ringo"]').should('not.be.checked').check().should('be.checked');
+    cy.get('[data-test="radio-result"]').contains('Ringo');
   });
 
   it('should find and control a color input', () => {
-    cy.get('[data-test="color-input"]');
-    cy.get('[data-test="color-result"]');
+    cy.get('[data-test="color-input"]').invoke('val', '#00ff00').trigger('input');
+    cy.get('[data-test="color-result"]').contains('#00ff00');
   });
 
   it('should find and control a date input', () => {
-    cy.get('[data-test="date-input"]');
-    cy.get('[data-test="date-result"]');
+    cy.get('[data-test="date-input"]').invoke('val', '2022-07-21').trigger('input');
+    cy.get('[data-test="date-result"]').contains('2022-07-21');
   });
 
   it('should find and control a range input', () => {
-    cy.get('[data-test="range-input"]');
-    cy.get('[data-test="range-result"]');
+    cy.get('[data-test="range-input"]').invoke('val', 8).trigger('input');
+    cy.get('[data-test="range-result"]').contains(8);
   });
 
   it('should find and control a file input', () => {
-    cy.get('[data-test="file-input"]');
-    cy.get('[data-test="file-result"]');
+    cy.get('[data-test="file-input"]').attachFile({
+      fileContent: 'some content',
+      mimeType: 'text/plain',
+      fileName: 'important-file.txt',
+    });
+    cy.get('[data-test="file-result"]').contains('important-file.txt');
   });
 });
