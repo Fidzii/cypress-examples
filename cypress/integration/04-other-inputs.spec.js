2e13fe24cfdf7e8c4cac8fa2ddc9397603f4658b
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-21 15:23:37 +0200

    04-other-inputs.spec.js

--- cypress/integration/04-other-inputs.spec.js
+++ cypress/integration/04-other-inputs.spec.js
@@ -8,9 +8,34 @@ describe('Secret Menu Items', () => {
     cy.get('#restaurant-visibility-filter').as('restaurant-filter');
   });
 
-  it('should set the range and verify it', () => {});
+  it('should set the range and verify it', () => {
+    cy.get('@rating-filter').invoke('val', 5).trigger('input').should('have.value', 5);
 
-  it('should check the checkbox and verify it', () => {});
+    cy.get('@rating-filter')
+      .invoke('val')
+      .then((val) => {
+        cy.get('[headers="popularity-column"] .cell')
+          .first()
+          .invoke('text')
+          .then(parseInt)
+          .should('be.at.least', Number(val));
+      });
+  });
 
-  it('should select an option from the select and verify it', () => {});
+  it('should check the checkbox and verify it', () => {
+    cy.get('[for="show-description"] input').should('be.checked').click().should('not.be.checked');
+
+    cy.get('#description-column').should('be.hidden');
+    cy.get('[headers="description-column"]').should('be.hidden');
+  });
+
+  it('should select an option from the select and verify it', () => {
+    cy.get('@restaurant-filter').select(1).as('selected');
+    cy.get('@selected')
+      .find('option:selected')
+      .invoke('text')
+      .then((text) => {
+        cy.get('.whereToOrder').each((element) => cy.wrap(element).should('have.text', text));
+      });
+  });
 });
