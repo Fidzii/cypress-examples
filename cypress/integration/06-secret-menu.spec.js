1f7a2b0dc3d54d74aca6ba7634c374efa9f982f6
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-22 13:18:04 +0200

    06-secret-menu.spec.j

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,28 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  for (const rating of ratings) {
+    it(`should set minimum rating: ${rating}`, () => {
+      cy.get('#minimum-rating-visibility').invoke('val', rating).trigger('input');
+      cy.get('td[headers="popularity-column"]').each((cell) =>
+        cy.wrap(cell).invoke('text').then(parseInt).should('be.at.least', rating),
+      );
+    });
+  }
+
+  for (const restaurant of restaurants) {
+    it(`should select restaurant: ${restaurant}`, () => {
+      cy.get('#restaurant-visibility-filter').select(restaurant);
+      cy.get('.whereToOrder > .cell').each((cell) => cy.wrap(cell).contains(restaurant));
+    });
+  }
+
+  for (const property of properties) {
+    it(`should uncheck property ${property}`, () => {
+      cy.get(`[for="show-${property}"]`).click();
+      cy.get(`#${property}-column`).should('be.hidden');
+      cy.get(`td.${property}`).should('be.hidden');
+    });
+  }
 });
