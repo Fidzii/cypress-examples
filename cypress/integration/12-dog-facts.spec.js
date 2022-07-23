01bdcb287bf82a6252c932610b965fa6a8d27695
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2022-07-23 11:20:55 +0200

    12-dog-facts.spec.js

--- cypress/integration/12-dog-facts.spec.js
+++ cypress/integration/12-dog-facts.spec.js
@@ -16,13 +16,38 @@ describe('Dog Facts', () => {
     cy.get('@emptyState');
   });
 
-  it('should make a request when the button is called', () => {});
+  it('should make a request when the button is called', () => {
+    cy.get('@fetchButton').click();
+    cy.wait('@api').should('exist');
+  });
 
-  it('should adjust the amount when the select is changed', () => {});
+  it('should adjust the amount when the select is changed', () => {
+    cy.get('@amountSelect').select('7 Facts').invoke('val').should('equal', '7');
+    cy.get('@fetchButton').click();
+    cy.wait('@api').then((int) => cy.wrap(int).its('request.url').should('contain', '?amount=7'));
+  });
 
-  it('should show the correct number of facts on the page', () => {});
+  it('should show the correct number of facts on the page', () => {
+    cy.get('@amountSelect').select('7 Facts').invoke('val').should('equal', '7');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+    cy.get('#facts article').should('have.length', 7);
+  });
 
-  it('should clear the facts when the "Clear" button is pressed', () => {});
+  it('should clear the facts when the "Clear" button is pressed', () => {
+    cy.get('@amountSelect').select('7 Facts').invoke('val').should('equal', '7');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+    cy.get('@emptyState').should('not.exist');
+    cy.get('@clearButton').click();
+    cy.get('@emptyState').should('exist');
+    cy.get('#facts article').should('have.length', 0);
+  });
 
-  it("should reflect the number of facts we're looking for in the title", () => {});
+  it("should reflect the number of facts we're looking for in the title", () => {
+    cy.get('@amountSelect').select('7 Facts').invoke('val').should('equal', '7');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+    cy.title().should('equal', `7 Dog Facts`);
+  });
 });
